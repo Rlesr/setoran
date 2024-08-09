05fea50020f44285f868b697580927a527957f15
Lietreum <mirsieolien@gmail.com>
2024-08-09 10:06:00 +0700

    Button navbar

--- fe/setoran/src/layouts/PageHeader.tsx
+++ fe/setoran/src/layouts/PageHeader.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import logoPT from '../assets/koperasiLogo.png';
+import React from "react";
+import logoPT from "../assets/koperasiLogo.png";
+import { Menu } from "lucide-react";
+import Button from "../components/Button";
 
-const PageHeader:React.FC = () => {
-    
-    return (
-    <div className='flex gap-10 lg:gap-20 justify-between'>
-    <div className='flex gap-4 items-center flex-shrink-0'>
-        <button></button>
-        <a href="/"> <img src={logoPT} className='h-6' /></a>
+const PageHeader: React.FC = () => {
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logoPT} className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
     </div>
-    <div></div>
-    <div></div>
-    </div>
-)
-}
+  );
+};
 export default PageHeader;
--- fe/setoran/tailwind.config.js
+++ fe/setoran/tailwind.config.js
@@ -1,14 +1,43 @@
-import daisyui from 'daisyui';
+import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: {
+          DEFAULT: colors.neutral[200],
+          hover: colors.neutral[300],
+          border: colors.neutral[400],
+          text: colors.neutral[500],
+          dark: colors.neutral[800],
+          ["dark-hover"]: colors.neutral[900],
+        },
+      },
+    },
   },
-  plugins: [daisyui],
-}
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": colors.blue[500],
+          "secondary": colors.neutral[200], // Custom secondary color
+          "accent": colors.blue[600],
+          "neutral": colors.neutral[500], // Custom neutral color
+          "base-100": colors.white,
+          "info": colors.blue[300],
+          "success": colors.green[500],
+          "warning": colors.yellow[500],
+          "error": colors.red[500],
+        },
+      },
+      // Optionally, you can include DaisyUI's default themes
+      'light',
+      'dark',
+    ],
+  },
+};
