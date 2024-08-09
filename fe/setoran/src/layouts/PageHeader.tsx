2aa59a872085e89d0a6f4cae1ec482ca73d62764
Lietreum <mirsieolien@gmail.com>
2024-08-09 10:53:54 +0700

    Button navbar

--- fe/setoran/src/layouts/PageHeader.tsx
+++ fe/setoran/src/layouts/PageHeader.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logoPT from "../assets/koperasiLogo.png";
-import { Menu } from "lucide-react";
+import { Bell, Menu, Moon, User } from "lucide-react";
 import Button from "../components/Button";
 
 const PageHeader: React.FC = () => {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
       <div className="flex gap-4 items-center flex-shrink-0">
         <Button variant="ghost" size="icon">
           <Menu />
@@ -15,7 +15,17 @@ const PageHeader: React.FC = () => {
         </a>
       </div>
       <div></div>
-      <div></div>
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button size="icon" variant="ghost">
+            <Moon />
+        </Button>
+        <Button size="icon" variant="ghost">
+            <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+            <User />
+        </Button>
+      </div>
     </div>
   );
 };
